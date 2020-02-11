a6487ee1f93f6eade0d15d114bfed123dd955c7b
Amanda Serex <amandaserex@ucsb.edu>
2020-02-10 23:55:17 -0800

    Outline created

--- quash.cpp
+++ quash.cpp
@@ -7,8 +7,30 @@
 
 using namespace std;
 
+void quash::insert(int key){
+	//“item successfully inserted, count = 1” or “item already present, new count =n”
 
+}
 
+void quash::lookup(int key){
+	//“item found, count = n” or “item not found”
+
+}
+
+void quash::deleteMin(){
+	//“min item = x, count decremented, new count = n” or “min item x successfully deleted” or “min item not present since table is empty”
+
+}
+
+void quash::Delete(int key){
+	//"item count decremented, new count = n” or “item successfully deleted” or “item not present in the table”
+
+}
+
+void quash::print(){
+	//prints out the heap array with elements separated by a whitespace.
+
+}
 
 
 int main(int argc, char* argv[]){
--- quash.h
+++ quash.h
@@ -1,1 +1,19 @@
 //quash.h
+#ifndef QUASH_H
+#define QUASH_H
+
+using namespace std;
+
+class quash{
+public:
+    void insert(int key); // Insert element i in both the heap and the hash table
+    void lookup(int key); // Use the hash table to determine if i is in the data structure.
+    void deleteMin(); //Use the heap to find the minimum element and decrement its count, return key that is deleted
+    void Delete(int key); //Use the hash table to determine where i is and decrement its count
+    void print(); //Print out the heap contents
+private:
+    pair<int,int> myHeap[100];//idk
+    pair<int,int> myHashTable[43];//idk
+};
+
+#endif
