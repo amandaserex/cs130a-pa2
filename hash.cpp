7900be1344a79cf0a8e42b9dba9d3a4d260e0d39
Amanda Serex <amandaserex@ucsb.edu>
2020-02-19 16:32:48 -0800

    update

--- hash.cpp
+++ hash.cpp
@@ -37,11 +37,18 @@ int Hash::currentCount(int key){
 }
 
 bool Hash::Delete(int key){
-    for(std::vector<std::pair<int, int> >::iterator m = table[key].begin(); m!= table[key].end();m++){
+    int myHashKey;
+	if(key<0){
+		myHashKey = (key%43)+43;
+	}
+	else{
+		myHashKey = key%43;
+	}
+    for(std::vector<std::pair<int, int> >::iterator m = table[myHashKey].begin(); m!= table[myHashKey].end();m++){
 		if(m->first==key){
             m->second=m->second-1;
             if(m->second==0){
-                table[key].erase(m);
+                table[myHashKey].erase(m);
                 return true; //it has been deleted
             }
         }
